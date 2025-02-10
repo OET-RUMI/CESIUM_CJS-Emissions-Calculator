28f7b001882d8d1fe91beae530a6c1f8484a3c53
Nathan Lannon <nathanlannon27@gmail.com>
2025-02-10 16:25:08 -0500

    cesium js template with webpack

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const cesiumSource = 'node_modules/cesium/Source';
+const cesiumWorkers = '../Build/Cesium/Workers';
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    context: __dirname,
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+		sourcePrefix: ''
+    },
+	amd: {
+		toUrlUndefined: true
+	},
+	resolve: {
+		alias: {
+			cesium: path.resolve(__dirname, cesiumSource)
+		},
+		fallback: {
+			fs: require.resolve("browserify-fs")
+		}
+	},
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [ 'style-loader', 'css-loader' ]
+        }, {
+            test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+            use: [ 'url-loader' ]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+		new CopyWebpackPlugin({ 
+            patterns: [
+                { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' },
+                { from: path.join(cesiumSource, 'Assets'), to: 'Assets' },
+                { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' }
+            ]
+        }),
+		new webpack.DefinePlugin({
+			CESIUM_BASE_URL: JSON.stringify('')
+		}),
+    ],
+	devServer: {
+        static: {
+			directory: path.join(__dirname, 'dist'),
+		},
+		open: true
+    },
+	mode: 'development'
+};
